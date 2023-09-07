6d871112ff6c8af45103291994c81d9f3c1af543
Jingwen Li <jingwen_li@mgic.com>
2023-09-07 10:36:41 -0500

    Added files from RateCalculator project

---
+++ RateCalculator/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace RateCalculator
+{
+    public partial class Form1 : Form
+    {
+        private string inputFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "CSV Files|*.csv";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    inputFilePath = ofd.FileName;
+                    label1.Text = "File selected: " + inputFilePath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputFilePath))
+            {
+                MessageBox.Show("Please select a file first.");
+                return;
+            }
+
+            try
+            {
+                List<string[]> rows = ReadCsvFile(inputFilePath);
+
+                if (rows.Count < 2)
+                {
+                    MessageBox.Show("No data found in the CSV file.");
+                    return;
+                }
+
+                // Validate the CSV columns.
+                if (rows[0].Length < 4)
+                {
+                    MessageBox.Show("The CSV file does not have the expected number of columns.");
+                    return;
+                }
+
+                // Inserting "loan_rate" header to CSV
+                rows[0] = rows[0].Concat(new[] { "loan_rate" }).ToArray();
+
+                for (int i = 1; i < rows.Count; i++)
+                {
+                    double pv = SafeConvertToDouble(rows[i][2]);
+                    double pmt = -SafeConvertToDouble(rows[i][3]);
+                    double nper = SafeConvertToDouble(rows[i][4]);
+
+                    var mortgage = new Mortgage(pv, pmt, nper);
+
+                    var rateValue = double.IsNaN(mortgage.Rate) ? "Not converged" : (mortgage.Rate * 12 * 100).ToString("F2");
+
+                    rows[i] = rows[i].Concat(new[] { rateValue }).ToArray();
+                }
+
+                string outputPath = inputFilePath.Replace(".csv", "_output.csv");
+                WriteCsvFile(outputPath, rows);
+
+                label1.Text = "Processing completed. \r\nOutput saved as " + outputPath;
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Error occurred: " + ex.Message;
+            }
+        }
+
+        private List<string[]> ReadCsvFile(string filePath)
+        {
+            var result = new List<string[]>();
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    result.Add(values);
+                }
+            }
+            return result;
+        }
+
+        private void WriteCsvFile(string filePath, List<string[]> data)
+        {
+            using (var writer = new StreamWriter(filePath))
+            {
+                foreach (var row in data)
+                {
+                    writer.WriteLine(string.Join(",", row));
+                }
+            }
+        }
+
+        private double SafeConvertToDouble(string input)
+        {
+            if (double.TryParse(input, out double result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new FormatException($"Could not convert '{input}' to a double.");
+            }
+        }
+    }
+}
---
+++ RateCalculator/Mortgage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RateCalculator
+{
+    public class Mortgage
+    {
+        private double Pv { get; set; }
+        private double Pmt { get; set; }
+        private double Nper { get; set; }
+        internal double Rate { get; set; }
+
+        public Mortgage(double pv, double pmt, double nper)
+        {
+            Pv = pv;
+            Pmt = pmt;
+            Nper = nper;
+            CalculateRate();
+        }
+
+        private void CalculateRate()
+        {
+            const double guess = 0.1;
+            const double tolerance = 1e-6;
+            const int maxIterations = 100;
+
+            int whenValue = 0;  // 'end' equivalent
+            double rn = guess;
+            int iterator = 0;
+            bool close = false;
+
+            while (iterator < maxIterations && !close)
+            {
+                double rnp1 = rn - GDivGp(rn, whenValue);
+                close = Math.Abs(rnp1 - rn) < tolerance;
+                iterator++;
+                rn = rnp1;
+            }
+
+            Rate = close ? rn : double.NaN;
+        }
+
+        private double GDivGp(double rn, int when)
+        {
+            double g = Pmt * (1 + rn * when) / rn * (Math.Pow(1 + rn, Nper) - 1) + Pv * Math.Pow(1 + rn, Nper);
+            double gp = Pv * Nper * Math.Pow(1 + rn, Nper - 1) + Pmt * when * (Math.Pow(1 + rn, Nper) - 1) / rn + Pmt * Nper * (1 + rn * when) / rn * Math.Pow(1 + rn, Nper - 1) - Pmt * (Math.Pow(1 + rn, Nper) - 1) / Math.Pow(rn, 2);
+            return g / gp;
+        }
+    }
+}
